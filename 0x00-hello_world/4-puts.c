6e15b462298de3c594eafe4770c1834ae8a563ea
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-17 05:38:26 -0800

    tth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * This is the documentation for the main function
+ * We are asked to do this by betty linter 
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
