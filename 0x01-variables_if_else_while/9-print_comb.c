436d95e412b97a468df927124d1dbb99a1991f8b
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 13:29:51 -0800

    jddj

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - shorts desc
+ * Return: Return value is 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
