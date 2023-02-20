e0568e414b4f3f759bfb5fa43d297d8c48b564bd
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 08:28:45 -0800

    jddjjd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -short desc
+ * Return: returns 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != q && c != e)
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
