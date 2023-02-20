41d69e9ae6d4cfc873ebb7ecb0758c04b263d68b
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 09:10:30 -0800

    jdkddkk

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - short desc
+ * Return: return value is 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
