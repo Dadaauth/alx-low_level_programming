1a879c0676539c8d5214bef539aaf51615b4cb46
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 08:02:37 -0800

    djdddj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - short desc
+ * Return: Return value is 0
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+	for (i = 0; str[i] != '\0'; i++) {
+		    putchar(str[i]);
+	}
+
+	return (0);
+}
