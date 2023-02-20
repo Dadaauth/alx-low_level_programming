959804507f4cc3cec96061a09b9776ed2637f986
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 02:54:04 -0800

    dkdd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(msg) - 1;
- 	ssize_t ret = write(STDERR_FILENO, msg, len);
+ 	write(STDERR_FILENO, msg, len);
 	return (1);
 }
