1fe2b41fad830532df8f9cc5df4805959f9a2089
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 13:48:58 -0800

    kddkk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,15 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		if (i == 9)
+
+		if (i != 9)
 		{
-			putchar(' ');
+			putchar(',');
 		}
-		else
-		{
-		        putchar(',');
-			putchar(' ');
-		}
-	}	
+		putchar(' ');
+	}
+	
+	putchar('\n');
 
 	return (0);
 }
