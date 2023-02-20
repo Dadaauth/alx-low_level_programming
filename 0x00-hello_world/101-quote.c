36718988e9b278189a5c7697b9cd1d3f05e01cd1
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-20 02:57:35 -0800

    dfks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - short description 
+ * main - short description
  * Return: Return description
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char msg[] = 
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(msg) - 1;
- 	write(STDERR_FILENO, msg, len);
+	write(STDERR_FILENO, msg, len);
+	
 	return (1);
 }
