2650fecc8b83f988d138c4a4c01f39cb66694de3
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-21 15:36:46 -0800

    jfhfo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - this prints the alphabet
+ */
+void print_alphabet(void)
+{
+	int c = 'a';
+
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
