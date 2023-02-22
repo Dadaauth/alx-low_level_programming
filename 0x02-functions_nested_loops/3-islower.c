6d51768078fd95d6d18833808ebb07ca25b2e810
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-02-21 16:12:44 -0800

    dkjddj

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _islower - checks if the given value is in lowercase
- * Return: return 1 if c is in lowercase 
+ * @c: the value to be tested
+ * Return: return 1 if c is in lowercase
  * and 0 otherwise
  */
 int _islower(int c)
