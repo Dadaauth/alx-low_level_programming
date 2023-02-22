8d9b63b53383786cfc61a38016e438d3711de044
dadaauth <dadaauthourity23@gmail.com>
2023-02-21 17:05:07 -0800

    dkjddj

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * test_sign - to test the code
+ * @n: The value to be tested
+ */
+
+int test_sign(int n)
+{
+	int r;
+
+	r = print_sign(n);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+}
+/**
+ * main - the main function
+ * Return: Returns 0.
+ */
+int main(void)
+{
+	test_sign(98);
+	test_sign(0);
+	test_sign(0xff);
+	test_sign(-1);
+	
+	return (0);
+}
