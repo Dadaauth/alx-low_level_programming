19ac4db09e15001e16a31e4a182437ba4ef48830
dadaauth <dadaauthourity23@gmail.com>
2023-02-21 17:30:59 -0800

    dkjddj

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: the value to be worked on
+ * Return: return value is based on input
+ */
+int _abs(int c)
+{
+	/** int rv;
+	 */
+
+	if (c < 0)
+	{
+		return (-c);	
+	}
+	else
+	{
+		return (c);
+	}
+
+}
