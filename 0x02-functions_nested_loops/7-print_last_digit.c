d617b64a6776ab6965b255a81891f0317b1cb73c
dadaauth <dadaauthourity23@gmail.com>
2023-02-21 17:50:18 -0800

    dkjddj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: the number to be tested
+ * Return: returns the value of the last digit
+ */
+int print_last_digit(int c)
+{
+
+	int last_digit = c % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	_putchar('0' + last_digit);
+	return last_digit;
+}
