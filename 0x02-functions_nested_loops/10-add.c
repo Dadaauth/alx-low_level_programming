f9d5067ea00c7cb4ffcee57733e4156faac6ce7a
dadaauth <dadaauthourity23@gmail.com>
2023-02-23 04:11:46 -0800

    dkjddj

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * add - adds two intgers 
+ * add - adds two intgers
+ * @a: first value
+ * @b: second value
  * Return: the return value is the addition of the int's
  */
 int add(int a, int b)
