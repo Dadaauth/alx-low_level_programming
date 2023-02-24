26cbbf9ce9bc3d7d9e11c5d3a7d346e3dc8bcc19
dadaauth <dadaauthourity23@gmail.com>
2023-02-24 06:54:17 -0800

    dkjddj

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_line - draws a straight line in the terminal 
+ * print_line - draws a straight line in the terminal
+ * @n: the value to determine the lines printed
  * based on the value of n
  */
 void print_line(int n)
