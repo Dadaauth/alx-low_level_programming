866b853c0ef5ee2989f3260ed206029e9e70acda
dadaauth <dadaauthourity23@gmail.com>
2023-02-28 07:29:16 -0800

    dkjddj

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - checks the code
+ * Return: 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers, I fear the lack of them - Issac Asimov";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*8
+ * _puts - Prints a String followed by a new line to the stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
