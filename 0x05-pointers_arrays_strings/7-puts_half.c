9c9122434b10ce931679096892f6b5d84a3ca27c
dadaauth <dadaauthourity23@gmail.com>
2023-02-28 11:12:39 -0800

    dkjddj

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string to work on.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+	int len2;
+
+	while (str[len] != '\0') 
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		len2 = len / 2;
+		while (str[len2] != '\0')	
+		{
+			_putchar(str[len2]);
+			len2++;
+		}
+	}
+	else if (len % 2 != 0)
+	{
+		len2 = (len - 1) / 2;
+		while (str[len2] != '\0')
+		{
+			_putchar(str[len2]);
+			len2++;
+		}
+	}
+	_putchar('\n');
+}
