7154c7a0c62ab77ea9f03a50be55039de50b6dea
dadaauth <dadaauthourity23@gmail.com>
2023-03-02 23:10:58 -0800

    dkjddj

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main -check the code
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
