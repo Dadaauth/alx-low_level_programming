809ccd9f5edf5bb178a1ba44fc6e1ae5b838253a
dadaauth <dadaauthourity23@gmail.com>
2023-03-08 11:45:09 -0800

    dkjddj

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * count_length - counts the length of a string
+ * @s: the string to count
+ * @i: just an integer with the value zero
+ * changes with each recursive call
+ * Return: The length of the string
+ */
 int count_length(char *s, int i)
 {
 	if (*(s + i) == '\0')
@@ -7,6 +14,11 @@ int count_length(char *s, int i)
 	i++;
 	return (count_length(s, i));
 }
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to work on
+ * Return: the length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	return (count_length(s, 0));
