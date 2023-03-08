96e81d03c7e13c17381684193ec9258dbd93be84
dadaauth <dadaauthourity23@gmail.com>
2023-03-08 14:11:29 -0800

    dkjddj

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,10 @@
  * Return: the square root of the number
  * returns 0 if n == 0, -1 id n < 0 and if n2 == 0
  */
-int get_sqrt(int n, long unsigned int n2)
+int get_sqrt(int n, unsigned long int n2)
 {
-	long unsigned int nst = n;
+	unsigned long int nst = n;
+
 	if (n == 0)
 		return (0);
 	if (n < 0)
@@ -30,13 +31,13 @@ int get_sqrt(int n, long unsigned int n2)
  */
 int _sqrt_recursion(int n)
 {
-	long unsigned int n2;
+	unsigned long int n2;
 
 	if (n > 100000 && n < 999999)
 		n2 = n / 50;
 	else if (n > 999999)
 		n2 = n / 100;
 	else
-		n2 = n; 
+		n2 = n;
 	return (get_sqrt(n, n2));
 }
