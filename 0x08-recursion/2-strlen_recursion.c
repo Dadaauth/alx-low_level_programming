9d8c488e06afa773f015bc56a8f5442a093aaa2e
dadaauth <dadaauthourity23@gmail.com>
2023-03-08 11:40:54 -0800

    dkjddj

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int count_length(char *s, int i)
+{
+	if (*(s + i) == '\0')
+		return (i - 1);
+	i++;
+	return (count_length(s, i));
+}
+int _strlen_recursion(char *s)
+{
+	return (count_length(s, 0));
+}
