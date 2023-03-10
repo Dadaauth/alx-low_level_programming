3dc5f9594c6237dc46a9212c539aba6a35534b9b
dadaauth <dadaauthourity23@gmail.com>
2023-03-10 15:17:52 -0800

    dkjddj

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 
 int _islower(int c);
@@ -42,4 +41,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 
-
+#endif
