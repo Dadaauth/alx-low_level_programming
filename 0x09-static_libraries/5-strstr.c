fcdeb34820f448a8a178c31a6dc1acdf4fd926f9
dadaauth <dadaauthourity23@gmail.com>
2023-03-10 14:32:31 -0800

    dkjddj

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -9,46 +9,5 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k, len_ned;
-	int isAvailable;
-	char *f;
-
-	len_ned = 0;
-	for (k = 0; *(needle + k) != '\0'; k++)
-	{
-		len_ned++;
-	}
-	if (len_ned == 0)
-	{
-		return (haystack);
-	}
-
-	for (i = 0; *(haystack + i) != '\0'; i++)
-	{
-		if (*(haystack + i) == *(needle))
-		{
-			for (j = 0; *(needle + j) != '\0'; j++)
-			{
-				if (*(needle + j) == *(haystack + i + j))
-				{
-					f = (haystack + i);
-					isAvailable = 1;
-				}
-				else
-				{
-					isAvailable = 0;
-					break;
-				}
-			}
-			break;
-		}
-	}
-	if (isAvailable)
-	{
-		return (f);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return ("ddnk");
 }
