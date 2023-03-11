246964b26c5219cec571f10c3af9fe5abcdea372
dadaauth <dadaauthourity23@gmail.com>
2023-03-11 01:47:59 -0800

    dkjddj

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+ * check_all_digit - checks all the digit of the arguements if they are numbers
+ * @argc: the number of arguements
+ * @argv: an array of the arguements
+ * Return: 0 if all are numbers and 1 if not all are numbers
+ */
+int check_all_digit(int argc, char *argv[])
+{
+	int i;
+	unsigned int j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				continue;
+			else
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
 /**
  * main - prints the addition of positive arguements
  * @argc: the number of arguements
@@ -11,29 +37,26 @@ int main(int argc, char *argv[])
 	int i;
 	long int sum;
 	long int argvlong[98];
-	
+
 	if (argc == 1)
 		printf("0\n");
 	else if (argc > 1)
 	{
+		if (check_all_digit(argc, argv) == 1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
 		if (argc > 99)
 		{
 			printf("Maximum arguements  is 98, result might not be correct!!");
-		}
-		for (i = 0; i < argc; i++)
-		{
-			argvlong[i] = atoi(argv[i]);
-
-			if (argvlong[i] !>= '0' || argvlong[i] !<= '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
 		}
 
 		sum = 0;
 		for (i = 0; i < argc; i++)
 		{
+			argvlong[i] = atoi(argv[i]);
 			sum = sum + argvlong[i];
 		}
 		printf("%ld\n", sum);
