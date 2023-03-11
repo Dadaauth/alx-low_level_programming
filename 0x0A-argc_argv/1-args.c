71a8dcdc98a38484a6bbcbc4533a5d54145c468d
dadaauth <dadaauthourity23@gmail.com>
2023-03-11 00:32:25 -0800

    dkjddj

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguements
+ * passed into the program
+ * @argc: number of arguements
+ * @argv: an array of arguements
+ * Return: Returns success
+ */
+int main (int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	exit (EXIT_SUCCESS);
+}
