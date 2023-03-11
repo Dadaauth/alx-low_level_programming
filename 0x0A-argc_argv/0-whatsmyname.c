4b3701d12eec22ebde8d13ab18f7cfd542d20912
dadaauth <dadaauthourity23@gmail.com>
2023-03-11 00:25:15 -0800

    dkjddj

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - gets the name of the program
+ * @argc: the number of arguements on the command line
+ * @argv: the arguements in an array
+ * Return: returns success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}
