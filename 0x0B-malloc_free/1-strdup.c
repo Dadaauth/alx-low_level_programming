d1a3ba27f2d8085364b06e980a47f504835b6559
dadaauth <dadaauthourity23@gmail.com>
2023-03-13 10:35:34 -0700

    second project correction:: The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * string_len - gets the length of the given string
  * @str: the string to test
  * @n: just the number 0.
+ * Return: the length of the string
  */
 unsigned int string_len(char *str, int n)
 {
@@ -21,15 +23,16 @@ unsigned int string_len(char *str, int n)
  */
 char *_strdup(char *str)
 {
-	char *ptr ;
+	char *ptr;
 	unsigned int i;
 	unsigned int str_len;
 
-	if(!str)
+	if (!str)
 		return (NULL);
 	str_len = string_len(str, 0);
-	ptr = malloc(str_len * sizeof(char));
-
+	ptr = malloc(str_len + 1 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < str_len + 1; i++)
 	{
 		ptr[i] = str[i];
