62caec8072173b07106ef048cdf3ae433f4a506e
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-23 13:47:51 +0100

    correction to project:  If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,14 +12,27 @@
 int **alloc_grid(int width, int height)
 {
 	int **array;
-    int i, j;
+	int i, j;
 
-    array = malloc(sizeof(int *) * height);
-    for (i = 0; i < height; i++)
-    {
-        array[i] = malloc(sizeof(int) * width);
-        for (j = 0; j < width; j++)
-            array[i][j] = 0;
-    }
-    return (array);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			free(array[i]);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+	return (array);
 }
