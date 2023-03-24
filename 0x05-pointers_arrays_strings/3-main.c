bb8d576c6bb759432c819e86860cacb72e14bc5b
dadaauth <dadaauthourity23@gmail.com>
2023-03-24 11:38:04 -0700

    fourth project

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear the use of computers. I am destined to become an inventor!!";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string followed by a newline to the stdout
+ * @str: the string to print
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		_putchar(*(str + i));
+}
