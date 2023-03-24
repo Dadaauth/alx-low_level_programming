fffe58c136d1fdf5565d8c700a327a696f40ff79
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-24 14:24:31 +0100

    Project First Attempt: Beauty is variable, ugliness is constant1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * sum_them_all - adds all the arguements given together
+ * @n: number of arguments given
+ * Return: sum of all the arguments, zero(0) if no arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list arg_ptr;
+    unsigned int i; 
+    int sum = 0;
+
+    if (n == 0) return (0);
+
+    va_start(arg_ptr, n);
+    for (i = 0; i < n; i++)
+        sum += va_arg(arg_ptr, int);
+    va_end(arg_ptr);
+
+    return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Alx Variadic Function Project
