2e687d69658a3e4cc075aa5e343806c5429df868
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-24 15:37:09 +0100

    correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list arg_ptr;
-    unsigned int i;
-    int sum = 0;
+	va_list arg_ptr;
+	unsigned int i;
+	int sum = 0;
 
-    if (n == 0)
-        return (0);
-    va_start(arg_ptr, n);
-    for (i = 0; i < n; i++)
-        sum += va_arg(arg_ptr, int);
-    va_end(arg_ptr);
-    return (sum);
+	if (n == 0)
+		return (0);
+	va_start(arg_ptr, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(arg_ptr, int);
+	va_end(arg_ptr);
+	return (sum);
 }
