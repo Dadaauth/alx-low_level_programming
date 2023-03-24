4a1dc85a847dcdffc8c564a99b34d3d596299073
dadaauth <dadaauthourity23@gmail.com>
2023-03-24 11:07:01 -0700

    first project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_9 - updates the value of an integer to 98 using pointers
+ * @n: a pointer to the variable to change
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
