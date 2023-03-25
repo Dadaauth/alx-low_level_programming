80e7aed61f920d63b6fa28146c5e0f9d8509968e
dadaauth <dadaauthourity23@gmail.com>
2023-03-25 04:22:42 -0700

    Eigth Project

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string to print
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len])
+		len++;
+	if (len % 2 == 0)
+		n = len / 2;
+	else if (len % 2 != 0)
+		n = (len -1) / 2;
+	i = len - n;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
