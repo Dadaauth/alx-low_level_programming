cc4344f719d2f0d21bf9bcf5e3a3d06f88131d93
dadaauth <dadaauthourity23@gmail.com>
2023-03-25 04:40:30 -0700

    tenth project

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+#include "main.h"
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: where to copy the string to
+ * @src: the string to copy
+ * Return: A pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
