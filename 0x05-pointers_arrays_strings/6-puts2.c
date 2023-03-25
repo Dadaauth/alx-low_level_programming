69bf64502809822784b9a089427530044a7d213e
dadaauth <dadaauthourity23@gmail.com>
2023-03-25 03:15:05 -0700

    seventh project

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character in a string
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
