dfac6a89f98c55eb4ef77c74a2a6af1382b6e512
dadaauth <dadaauthourity23@gmail.com>
2023-03-25 01:26:45 -0700

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,12 @@
 void rev_string(char *s)
 {
 	char temp;
-	int i, len = 0;
+	int i, len = 0, len2;
 
 	while (*(s + len) != '\0')
 		len++;
-
-	for (i = 0; i < len/2; i++)
+	len2 = len;
+	for (i = 0; i < len2/2; i++)
 	{
 		temp = s[i];
 		s[i] = s[len - 1];
