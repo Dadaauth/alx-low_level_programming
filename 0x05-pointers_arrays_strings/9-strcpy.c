b470ab202c9a0eb39fa69e88741a47077b569943
dadaauth <dadaauthourity23@gmail.com>
2023-03-25 04:58:43 -0700

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strcpy - copies a string from src to dest
+ * _strcpyi - copies a string from src to dest
  * @dest: where to copy the string to
  * @src: the string to copy
  * Return: A pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpyi(char *dest, char *src)
 {
 	int i;
 
