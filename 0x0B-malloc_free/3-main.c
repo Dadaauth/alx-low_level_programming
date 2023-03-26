2c99bab0cd21d3516426d723a071344ef01f638b
dadaauth <dadaauthourity23@gmail.com>
2023-03-26 09:36:00 -0700

    project - It's not bragging if you can back it up

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -38,17 +38,16 @@ int main(void)
 {
     int **grid;
 
-    grid = alloc_grid(20, 20);
+    grid = alloc_grid(6, 4);
     if (grid == NULL)
     {
-        printf("Grid returns 1");
         return (1);
     }
-    print_grid(grid, 20, 20);
+    print_grid(grid, 6, 4);
     printf("\n");
     grid[0][3] = 98;
     grid[3][4] = 402;
-    print_grid(grid, 20, 20);
-    free(grid);
+    print_grid(grid, 6, 4);
+    free_grid(grid, 4);
     return (0);
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees a two dimensional array created with malloc() function
+ * @grid: a pointer to pointer of the array to free
+ * @height: the height of the array
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
