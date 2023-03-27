22cb65c368454f81267a1b0f6abd69c1ea4ae9e5
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-27 16:35:10 +0100

    correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,7 +42,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL && new_size == 0)
-		return (ptr);
+		return (NULL);
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
