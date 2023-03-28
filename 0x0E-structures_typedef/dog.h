fec9d29c27e069bd451b7e1556a5005a2b31a95e
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-28 19:44:19 +0100

    project - Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a struct for dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
