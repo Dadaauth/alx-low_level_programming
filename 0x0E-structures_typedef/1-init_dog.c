dfd4ac76378205a4e476c3d71c319cc288fdae03
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 11:34:12 +0100

    correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		exit(EXIT_SUCCESS);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)	
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 }
