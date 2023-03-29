f35347849d177ae94e0d467ce8dd00a6cc1baffe
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 10:37:22 +0100

    project -  If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * array_iterator - class a function on each element of an array
+ * @array: the array to work on
+ * @size: the size of the array
+ * @action: the function to call
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
