e629f871783bbd04050091849d60b460ceb9d6dd
dadaauth <dadaauthourity23@gmail.com>
2023-03-29 04:42:31 -0700

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * copy_str - copies strings
+ * @s1: string 1
+ * @s2: string 2
+ * @s3: third string
+ */
+void copy_str(dog_t *s1, char *s2, char *s3)
+{
+	int i;
+
+	s1->name = s2;
+	s1->owner = s3;
+	for (i = 0; *(s2 + i); i++)
+	{
+		
+	}
+
+}
+/**
  * new_dog - creates a new dog
  * @name: name for the dog
  * @age: age for the dog
@@ -18,9 +36,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(newdog);
 		return (NULL);
 	}
-	strcpy(newdog->name, name);
+	
+	copy_str(newdog, name, owner);
 	newdog->age = age;
-	strcpy(newdog->owner, owner);
 
 	return (newdog);
 }
