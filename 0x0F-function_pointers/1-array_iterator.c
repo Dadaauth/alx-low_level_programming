1cf5964c5ea507087ee2a0f3f119a0c4a3e7b27b
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 10:40:41 +0100

    project  If you spend too much time thinking about a thing, you'll never get it done;; changelog:: Added checking if NULL value is passed to the function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * array_iterator - class a function on each element of an array
  * @array: the array to work on
@@ -9,7 +10,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
+	if (array == NULL || action == NULL)
+		exit(EXIT_SUCCESS);
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
