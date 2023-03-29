6d62e9e57f941585e52cb65193c14ebacde4513f
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 10:59:28 +0100

    project - To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * int_index - a fucntion that searches for an integer
+ * @array: the array to work on
+ * @size: the size of the array
+ * @cmp: the function to call
+ * Return: returns the index of the first element for which
+ * the cmp function does not return zero(0);
+ * description: A function that searches for an integer
+ * using  the concept of function pointers
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, rtn_val;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		exit(EXIT_SUCCESS);
+	for (i = 0; i < size; i++)
+	{
+		rtn_val = cmp(array[i]);
+		if (rtn_val != 0)
+			break;
+	}
+	if (i == size)
+		i = -1;
+	return (i);
+}
