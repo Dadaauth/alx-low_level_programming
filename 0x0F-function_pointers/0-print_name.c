b806bdde0ba01f38659c9a597aff458c5aaf676e
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 10:00:09 +0100

    project - What's my name?

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_name - prints a name using the concept of function pointers
+ * @name: the name to print
+ * @f: a pointer to a function to call
+ * Return: nothing.
+ */
+void print_name(char *name, char (*f)(char *))
+{
+	f(name);
+}
