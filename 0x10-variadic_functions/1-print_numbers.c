0e0d45b196a88c143c02bef18a3070635c888057
dadaauth <dadaauthourity23@gmail.com>
2023-03-29 08:02:07 -0700

    project - To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdargs.h>
+/**
+ * print_numbers - prints numbers using the
+ * concept of variadic functions
+ * @seperator: to be put between printed numbers
+ * @n: number of arguements passed
+ * @...: variadic function arguements
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list arg_ptr;
+	int i;
+
+	if (seperator != NULL)
+	{
+		va_start(arg_ptr, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d",va_arg(arg_ptr, int));
+			printf("%s", seperator);
+		}
+		printf("\n");
+	}
+}
