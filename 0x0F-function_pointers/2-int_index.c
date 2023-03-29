1ef3fb029c3bb4c466829422ab9b79fbf4dd5f1d
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-29 11:01:17 +0100

    correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
-		exit(EXIT_SUCCESS);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		rtn_val = cmp(array[i]);
