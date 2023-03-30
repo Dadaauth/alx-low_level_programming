355a076610e804adf187768e73bdf3fb116605df
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-03-31 00:49:31 +0100

    project - Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdbool.h>
+/**
+ * print_list - prints the elements of a linked list
+ * @h: the list to print
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *current = (list_t *) h;
+	long unsigned int node_count = 0;
+	bool continue_loop = true;
+
+	while (continue_loop)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)");
+		else
+		{
+			printf("[%u] %s", current->len, current->str);
+		}
+		node_count++;
+		if (current->next == NULL)
+			continue_loop = false;
+		else
+		{
+			current = current->next;
+			printf("\n");
+		}
+	}
+	printf("\n");
+	return (node_count);
+}
