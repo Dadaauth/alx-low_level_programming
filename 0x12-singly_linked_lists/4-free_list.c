3026762e3ef6a1dfbec770527f421e4fd61607f5
dadaauth <dadaauthourity23@gmail.com>
2023-03-30 21:21:52 -0700

    project - Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,17 +8,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp = head;
-	list_t *temp2;
+	list_t *temp;
 
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (temp->next != NULL)
-		{
-			temp2 = temp;
-			free(temp);
-			temp = temp2->next;
-		}
+		temp = head;
+		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 }
