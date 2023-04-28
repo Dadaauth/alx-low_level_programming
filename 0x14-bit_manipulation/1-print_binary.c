287ec871b31b5a3490ecb0f5f9cfaa2065222b30
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:28:53 +0100

    dljf

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-
+/**
+ * print_binary - kfhdk
+ * @n: dkddj
+ */
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned long int mask;
-	int s = sizeof(unsigned long int);
 	int started = 0;
-
-	mask = 1UL << (s * 8 - 1);
-	for (i = 0; i < s * 8; i++)
+	for (int i = 63; i >= 0; i--)
 	{
-		if (n & mask)
+		if ((n >> i) & 1)
 		{
-			putchar('1');
 			started = 1;
+			putchar('1');
 		}
 		else if (started)
+		{
 			putchar('0');
-		mask >>= 1;
+		}
 	}
 	if (!started)
+	{
 		putchar('0');
-	putchar('\n');
+	}
 }
