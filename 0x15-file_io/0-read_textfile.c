27232dae3a142c9e47e421d6839cbd7e81ce23d8
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 12:26:58 +0100

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text file and prints it to stdout.
+ * @filename: the filename
+ * @letters: number of letter to read and print
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	int num_read;
+	char buffer[BUFSIZ];
+
+	while ((num_read = read(fd, buffer, letters)) > 0)
+	{
+		if (write(STDOUT_FILENO, buffer, num_read) != num_read)
+		{
+			return (0);
+		}
+	}
+
+	if (num_read == -1)
+		return (0);
+	close(fd);
+	return (num_read);
+}
