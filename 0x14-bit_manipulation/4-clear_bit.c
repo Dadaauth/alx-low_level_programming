166ebab86dd36d9792c184587d4309c85b226be6
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:19:36 +0100

    jkdf

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,8 @@
  * @index: flkd
  * Return: fdjkfjf
  */
-int clear_bit(unsigned long int *n, unsigned int index) {
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
