812ab9eeaa16343a9079281f587382c20407d70a
Dada Authority <dadaauthourity23@gmail.com>
2023-04-28 12:52:03 +0100

    second project

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+/**
+ * create_file - creates a file
+ * @filename: the filename
+ * @text_content: the content to put in the file
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		if ((size_t)written != strlen(text_content))
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
