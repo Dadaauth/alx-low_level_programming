120e3c19087d2cc5658395436741adf1423b01a4
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:04:28 +0100

    fjkf

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,12 +5,21 @@ void print_binary(unsigned long int n)
 	int i;
 	unsigned long int mask;
 	int s = sizeof(unsigned long int);
-	mask = 1UL << (s * 8 - 1);
+	int started = 0;
 
+	mask = 1UL << (s * 8 - 1);
 	for (i = 0; i < s * 8; i++)
 	{
-		putchar((n & mask) ? '1' : '0');
+		if (n & mask)
+		{
+			putchar('1');
+			started = 1;
+		}
+		else if (started)
+			putchar('0');
 		mask >>= 1;
 	}
+	if (!started)
+		putchar('0');
 	putchar('\n');
 }
