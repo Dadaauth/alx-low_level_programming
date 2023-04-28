f0cd889de16870b9080576da655e31a908e3d61a
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:34:27 +0100

    this is a commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-
-int get_bit(unsigned long int n, unsigned int index) {
+/**
+ * get_bit - dj,he
+ * @n: kjf
+ * @index: this is the index
+ * Return: something
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
 	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 	mask = 1UL << index;
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 }
