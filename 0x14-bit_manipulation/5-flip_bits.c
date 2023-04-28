e5d3124eeea3eef86bd5e9e4e6aae7439039f92d
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:23:34 +0100

    fkf.fjh

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,8 @@
  * @m: dkldjd
  * Return: fkldjkf
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m) {
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
 	unsigned int count = 0;
 	unsigned long int xor = n ^ m;
 
