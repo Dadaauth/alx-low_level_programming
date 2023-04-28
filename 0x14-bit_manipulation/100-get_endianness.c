2c2f612ccec780e0d7b563ed46d6f1c8d9ce6ab3
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-28 14:38:20 +0100

    kjd

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_endianness - fkfjhf
+ * Return: fkjdd
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *p = (char *)&n;
+	
+	return (*p == 1);
+}
