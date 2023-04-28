0c87e2d09f6ce2432a07580849cd807ea466fabb
Dada Authority <dadaauthourity23@gmail.com>
2023-04-28 12:32:19 +0100

    kfj

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,25 +11,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	int num_read;
+	char buffer[BUFSIZ];
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	int num_read;
-	char buffer[BUFSIZ];
 
 	while ((num_read = read(fd, buffer, letters)) > 0)
 	{
+		if (num_read == -1)
+			return (0);
+		printf("Read => %d \n", num_read);
 		if (write(STDOUT_FILENO, buffer, num_read) != num_read)
 		{
 			return (0);
 		}
 	}
-
-	if (num_read == -1)
-		return (0);
 	close(fd);
 	return (num_read);
 }
