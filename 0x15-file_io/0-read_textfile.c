4500d20aa334b0cd770695050f43aacd954ca6b7
Dada Authority <dadaauthourity23@gmail.com>
2023-04-28 12:34:59 +0100

    sjdd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	int num_read;
-	char buffer[BUFSIZ];
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
+	buffer = malloc(letters * sizeof(char));
 	while ((num_read = read(fd, buffer, letters)) > 0)
 	{
 		if (num_read == -1)
 			return (0);
-		printf("Read => %d \n", num_read);
+
 		if (write(STDOUT_FILENO, buffer, num_read) != num_read)
 		{
 			return (0);
