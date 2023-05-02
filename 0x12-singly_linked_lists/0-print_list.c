96473213f311a4fc7200cd74222c7ca0f39e925e
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-05-02 22:05:52 +0100

    linked lists project redo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,42 +1,25 @@
-#include "lists.h"
 #include <stdio.h>
-#include <stdbool.h>
+#include "lists.h"
 /**
- * print_list - prints the elements of a linked list
+ * print_list - prints all the element of a linked list
  * @h: the list to print
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	list_t *current = (list_t *) h;
-	unsigned int node_count = 0;
-	bool continue_loop = true;
+	size_t nOfNodes = 0;
+	const list_t *temp = h;
 
-	if (current != NULL)
+	if (h == NULL)
+		return (0);
+	while (temp != NULL)
 	{
-		while (continue_loop)
-		{
-			if (current->str == NULL)
-				printf("[0] (nil)");
-			else
-			{
-				printf("[%u] %s", current->len, current->str);
-			}
-			node_count++;
-			if (current->next == NULL)
-				continue_loop = false;
-			else
-			{
-				current = current->next;
-				printf("\n");
-			}
-		}
-	/**
-	 * You can also check if the next structure has
-	 * a NULL pointer so you stop the loop then print
-	 * the next structure contents after the loop
-	 */
-	printf("\n");
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		nOfNodes++;
 	}
-	return (node_count);
+	return (nOfNodes);
 }
