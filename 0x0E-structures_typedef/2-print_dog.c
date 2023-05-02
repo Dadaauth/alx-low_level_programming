9896b12f0f03b2414ebddbe89a94fb3936e6ed32
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-05-02 13:46:13 +0100

    df

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		exit(2);
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else	
 		printf("Name: %s\n", d->name);
-	printf("Age: %d\n", d->age);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil\n)");
 	else
 		printf("Owner: %s\n", d->owner);
 }
