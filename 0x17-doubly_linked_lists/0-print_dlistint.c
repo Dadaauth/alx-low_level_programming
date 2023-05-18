771f00b9425402ecef6d820749161eb314014a30
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-05-18 12:56:46 +0100

    error fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nofnodes = 0;
-	dlistint_t *temp = (dlistint_t) h;
+	dlistint_t *temp = (dlistint_t*) h;
 
 	while (temp != NULL)
 	{
-		print("%d", temp->n);
+		printf("%d", temp->n);
 		temp = temp->next;
 		nofnodes++;
 	}
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: the head of the linked list
+ * Return: the number of elements in the list
