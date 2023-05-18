e41458337c0b4b24b2151b7d933b564b88b031f7
Dada Authority <dadaauthourity23@gmail.com>
2023-05-18 13:22:48 +0100

    task 1 => List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,3 +5,16 @@
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: the head of the linked list
  * Return: the number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nofelements = 0;
+	dlistint_t *temp = (dlistint_t *)h;
+
+	while (temp != NULL)
+	{
+		nofelements++;
+		temp = temp->next;
+	}
+	return (nofelements);
+}
