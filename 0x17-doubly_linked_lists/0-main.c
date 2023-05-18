087da600874c1599a7e8a0d0421036752a72afc5
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-05-18 13:05:49 +0100

    Added new line to output

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -19,7 +19,7 @@ int main(void)
     new = malloc(sizeof(dlistint_t));
     if (new == NULL)
     {
-        dprintf(2, "Error: Can't malloc\n");
+        printf("Error => Can't malloc\n");
         return (EXIT_FAILURE);
     }
     new->n = 9;
@@ -28,7 +28,7 @@ int main(void)
     new->prev = NULL;
     head = new;
     n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
+    printf("-> %lu elements\n", (unsigned long)n);
     free(new);
     return (EXIT_SUCCESS);
 }
