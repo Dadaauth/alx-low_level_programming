0fe47dafd71e0929bce11480dd7539c7cdbc5708
Dada Authority <dadaauthourity23@gmail.com>
2023-06-13 09:06:10 +0100

    Fixed some betty / coding errors...

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	long unsigned int hash = hash_djb2((unsigned char *)key);
+	unsigned long int hash = hash_djb2((unsigned char *)key);
 
 	return (hash % size);
 }
