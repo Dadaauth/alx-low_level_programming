5cfa9a9628bac07a49cf23ee89872330392b6493
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-03-01 18:20:34 +0100

    task #3 done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
+void print_array(int *arr, size_t size);
 /**
- * binary_search - searches a sorted array of integers using 
+ * binary_search - searches a sorted array of integers using
  * the Binary search algorithm
  * @array:  a pointer to the first element in the array to search from
  * @size: the length of the array
@@ -9,50 +11,69 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t midpoint = (size / 2), i;
-	int *tmpArray = array;
+	size_t midpoint, start = 0, stop = size - 1, newsize = size;
+	int *tmpArr = array;
 
-	if (array == NULL)
+	if (tmpArr == NULL || size == 0)
 		return (-1);
-	printf("Searching in array (midpoint = %ld): ", midpoint);
+
+	while (true)
+	{
+		if (start == stop)
+		{
+			if (tmpArr[start] == value)
+				return (start);
+			else
+				return (-1);
+		}
+		newsize = (stop - start) + 1;
+		midpoint = newsize / 2;
+		midpoint--;
+		if ((newsize % 2) != 0)
+		{
+			midpoint++;
+		}
+		midpoint += start;
+
+		print_array(tmpArr + start, newsize);
+
+		if (tmpArr[midpoint] == value)
+			return (midpoint);
+		else if (tmpArr[midpoint] > value)
+		{
+			stop = midpoint - 1;
+			continue;
+		}
+		else if (tmpArr[midpoint] < value)
+		{
+			start = midpoint + 1;
+			continue;
+		}
+	}
+
+	return (-1);
+}
+
+/**
+ * print_array - prints an array of integers
+ * @arr: the array to print
+ * @size: the size of the array to print
+ * Return: Nothing
+ */
+void print_array(int *arr, size_t size)
+{
+	size_t i;
+
+	if (arr == NULL)
+		return;
+
+	printf("Searching in array: ");
 	for (i = 0; i < size; i++)
 	{
-		printf("%d", tmpArray[i]);
-		if (i != size - 1)
+		printf("%d", arr[i]);
+		if (i < (size - 1))
 			printf(", ");
 		else
 			printf("\n");
 	}
-
-	if (size == 0)
-		return (-1);
-
-	if (size == 1)
-	{
-		if (tmpArray[0] == value)
-			return (0);
-		else
-			return (-1);
-	}
-
-	if (tmpArray[midpoint] == value)
-	{
-		return (midpoint);
-	}
-	else if (tmpArray[midpoint] > value)
-	{
-		size = midpoint - 1;
-		return binary_search(tmpArray, size, value);
-	}
-	else if (tmpArray[midpoint] < value)
-	{
-		size = size - midpoint - 1;
-		for (i = 0; i < size; i++)
-		{
-			midpoint++;
-			tmpArray[i] = tmpArray[midpoint];
-		}
-		return (binary_search(tmpArray, size, value));
-	}
-	return (-1);
 }
